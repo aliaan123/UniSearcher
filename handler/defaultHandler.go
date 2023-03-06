cbec6d1406f28ba366153e6b51ad6267f1cddeb7
Aliaan <aliaan.azam02@gmail.com>
2023-03-06 16:25:09 +0100

    typo fixed in defaultHandler

--- handler/defaultHandler.go
+++ handler/defaultHandler.go
@@ -9,7 +9,7 @@ import (
 // DefaultHandler function, which just prints info about the service
 func DefaultHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Hi! Here is some info in order to get started using this service:\n\t")
-	fmt.Fprintf(w, "\n\t For information about the country a specific university recides in; "+constants.UNI_INFO_PATH+"{your_search_name}")
-	fmt.Fprintf(w, "\n\t For information about universities in neightbouring countries with the same name; "+constants.NEIGHBOR_UNIS_PATH+"{country_search_name}"+"/{your_search_name")
+	fmt.Fprintf(w, "\n\t For information about the country a specific university recides in; "+constants.UNI_INFO_PATH+"{:partial_or_complete_university_name}")
+	fmt.Fprintf(w, "\n\t For information about universities in neightbouring countries with the same name; "+constants.NEIGHBOR_UNIS_PATH+"{:country_name}/{:partial_or_complete_university_name}{?limit={:number}}")
 	fmt.Fprintf(w, "\n\t For information about the diagnosis of the API; "+constants.DIAG_PATH+"")
 }
